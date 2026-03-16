4dc5e4f77985834120ebb3fb9ccea6d14d75181a
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-16 23:56:14 +0100

    Third commit

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -1,1 +1,6 @@
-print("Variables")
+# Purpose of the program is to compare the integer entered by the user with 100
+# Sample input: n compare with 100
+# n is the value entered by the user
+
+n = int(input("Please enter an integer number: "))
+print(n >= 100)
