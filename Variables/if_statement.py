0791fff9a8ca48b79c72180fc6026cf17ecb4209
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-17 01:13:14 +0100

    Refactor if_statement.py to improve code readability and formatting

--- Variables/if_statement.py
+++ Variables/if_statement.py
@@ -1,11 +1,14 @@
+# Simple program that takes three numbers from the user and outputs the largest number
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 n3 = int(input("Enter the third number: "))
 
+#Assume the (nl)largest number is n1(the first number)
 nl = n1
 
-if n2 > n1:
+if n2 > n1: 
     nl = n2
+
 if n3 > n1:
     nl = n3
 
