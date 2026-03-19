98530fc3ed18689227c7a640ad7a19ed408e2063
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-19 23:33:08 +0100

    Completed simple dragon game

---
+++ Variables/dragons_choice_game.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+health = int(input("In a scale of 1-100, what is your health: "))
+
+if health > 70:
+    print(name, ", You are ready for the battle ")
+elif health >= 30 and health <= 70:
+    print("Damn! ", name, ", it's risky, but you might survive")
+elif health < 30:
+    print("Run away! You need a potion")
