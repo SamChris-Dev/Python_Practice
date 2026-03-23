3fa306513a2400268fa410f117a03b934d2f38b5
Samwel Simule <s098153@student.tu.kielce.pl>
2026-03-23 22:05:42 +0100

    Fix punctuation in ticket_bot.py description in README.md

--- Variables/ticket_bot.py
+++ Variables/ticket_bot.py
@@ -1,4 +1,4 @@
-# a simple ticket bot that lets you know your ticket price and the movie you can watch
+# A simple ticket bot that lets you know your ticket price and the movie you can watch
 age = int(input("Please input your age: "))
 
 if age < 13:
