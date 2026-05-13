c5db57f8064befb5a8b0251a377e767a89ca831f
Samwel Simule <s098153@student.tu.kielce.pl>
2026-05-13 21:26:42 +0200

    Reintroduce module functionality: add suml and prodl functions with tests

--- Packages_and_modules/program/main.py
+++ Packages_and_modules/program/main.py
@@ -1,3 +1,5 @@
+from sys import path
+path.insert(0,'C:\\Users\\sam04\\Documents\\Python_Learning\\Python_Practice\\Packages_and_modules\\module')
 from module import suml, prodl
 
 zeroes = [0 for i in range(5)]
